09a553dbbbe5a7fa8849caff582bbab278df4bd9
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-03-22 21:08:38 +0600

    added cards with css

--- src/Components/Product.js/Product.js
+++ src/Components/Product.js/Product.js
@@ -6,8 +6,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h3>{name}</h3>
-            <h7>This is Product Price: {price}</h7>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller:{seller}</small></p>
+                <p><small>Ratings {ratings} stars</small></p>
+                <button className='btn-cart'>Add to Cart</button>
+            </div>
         </div>
     );
 };
